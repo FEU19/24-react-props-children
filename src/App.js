7c4fce8125e1a75c8fa15fea1d459645e2b06f9c
David Andersson <lejonmanen@gmail.com>
2020-04-28 10:29:47 +0200

    Bordered component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Bordered from './components/Bordered';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1> props.children demo </h1>
+            </header>
+            <main>
+                <Bordered>
+                    Hello!
+                </Bordered>
+
+                <Bordered>
+                    <ul>
+                        <li> äpple </li>
+                        <li> päron </li>
+                        <li> vindruvor </li>
+                    </ul>
+                </Bordered>
+            </main>
+        </div>
+    );
 }
 
 export default App;
