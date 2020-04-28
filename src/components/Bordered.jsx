dc51e18bd1088085c12c578188c2c8f2ba80c4dd
David Andersson <lejonmanen@gmail.com>
2020-04-28 10:53:32 +0200

    Comments

--- src/components/Bordered.jsx
+++ src/components/Bordered.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// The content of the <Bordered> tag can be anything.
+// The CSS is defined in App.css
 const Bordered = ({ children }) => (
     <div className="bordered"> {children} </div>
 )
